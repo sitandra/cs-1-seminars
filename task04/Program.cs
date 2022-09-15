581be76cc1d33b6d98beffe44252209f74c55568
Julia Debelova <u.deb@mail.ru>
2022-09-15 23:43:10 +0500

    result task04

---
+++ task04/Program.cs
@@ -0,0 +1,7 @@
+﻿int userNumber;
+Console.Write("Введите целое трехзначное число: ");
+while (!int.TryParse(Console.ReadLine(), out userNumber) || userNumber < 100 || userNumber > 999)
+{
+    Console.Write("Нет, нужно ввести целое трехзначное число! Повторите ввод: ");
+}
+Console.WriteLine(userNumber % 10);
