0c36669da572022ede384ab7fee3cab50e8ac3a1
Julia Debelova <u.deb@mail.ru>
2022-09-15 22:49:16 +0500

    result task00

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int userNumber;
+if (int.TryParse(Console.ReadLine(), out userNumber))
+{
+    Console.WriteLine(Math.Pow(userNumber, 2));
+}
+else
+{
+    Console.WriteLine("Нужно ввести число");
+}
