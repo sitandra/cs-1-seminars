7d72420a26a6da09e3671e60dc64161da2a3bc36
Julia Debelova <u.deb@mail.ru>
2022-09-16 00:02:42 +0500

    result dz02

---
+++ dz02/Program.cs
@@ -0,0 +1,14 @@
+﻿int userNumber;
+Console.Write("Введите целое число: ");
+while (!int.TryParse(Console.ReadLine(), out userNumber))
+{
+    Console.Write("Нет, нужно ввести целое число! Повторите ввод: ");
+}
+if (userNumber % 2 == 0)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
