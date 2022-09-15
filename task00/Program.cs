600f4565597bebd83fe7114a9809c6b844bc12e3
Julia Debelova <u.deb@mail.ru>
2022-09-15 23:27:57 +0500

    add comment to task00

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите число: ");
 int userNumber;
-if (int.TryParse(Console.ReadLine(), out userNumber))
+if (int.TryParse(Console.ReadLine(), out userNumber)) // Convert.ToInt32();
 {
     Console.WriteLine(Math.Pow(userNumber, 2));
 }
