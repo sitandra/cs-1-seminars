0fe08f7485e4c83eb09853bd3a099ad1bfcdf70b
Julia Debelova <u.deb@mail.ru>
2022-09-16 00:09:29 +0500

    result dz03

---
+++ dz03/Program.cs
@@ -0,0 +1,22 @@
+﻿int userNumber;
+Console.Write("Введите целое число больше 1: ");
+while (!int.TryParse(Console.ReadLine(), out userNumber) || userNumber < 1)
+{
+    Console.Write("Нет, нужно ввести целое число больше 1! Повторите ввод: ");
+}
+bool flag = false;
+for (int i = 1; i <= userNumber; i ++)
+{
+    if (i % 2 == 0)
+    {
+        if (flag)
+        {
+            Console.Write(", ");
+        }
+        else
+        {
+            flag = true;
+        }
+        Console.Write(i);
+    }
+}
