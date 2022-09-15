4b35040790784ae96c79d062aea361c294e983a2
Julia Debelova <u.deb@mail.ru>
2022-09-15 23:45:34 +0500

    result dz00

---
+++ dz00/Program.cs
@@ -0,0 +1,13 @@
+﻿int userNumber1, userNumber2;
+Console.Write("a = ");
+while (!int.TryParse(Console.ReadLine(), out userNumber1))
+{
+    Console.Write("Нет, нужно ввести число! a = ");
+}
+Console.Write("b = ");
+while (!int.TryParse(Console.ReadLine(), out userNumber2))
+{
+    Console.Write("Нет, нужно ввести число! b = ");
+}
+Console.Write("max = ");
+Console.Write(Math.Max(userNumber1, userNumber2));
