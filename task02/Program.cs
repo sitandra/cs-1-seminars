e4ca859dff5a3ff14c589cd73d66980e402cd6f6
Julia Debelova <u.deb@mail.ru>
2022-09-15 23:20:59 +0500

    result task02

---
+++ task02/Program.cs
@@ -0,0 +1,8 @@
+﻿int weekDay;
+Console.Write("Введите номер дня недели: ");
+while (!int.TryParse(Console.ReadLine(), out weekDay) || weekDay < 1 || weekDay > 7)
+{
+    Console.Write("Нет, нужно ввести число от 1 до 7! Повторите ввод номера дня недели: ");
+}
+string[] week = new string[7] {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.WriteLine(week[weekDay - 1]);
