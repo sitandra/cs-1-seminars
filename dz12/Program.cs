1479aef08165d74c2909436c24238e2b31d3c277
Julia Debelova <u.deb@mail.ru>
2022-09-20 13:04:31 +0500

    add dz12 from seminar 2 about functions and array: check weekends from input number of week day

---
+++ dz12/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int getReadLineWeekDayNumber()
+{
+    int number;
+    string message = "Введите номер дня недели (1 – 7)";
+    Console.Write($"{message}: ");
+    while (!int.TryParse(Console.ReadLine(), out number) || number < 1 || number > 7)
+    {
+        Console.Write($"Нет! {message}: ");
+    }
+    return number;
+}
+
+bool isWeekend(int weekDayNumber)
+{
+    int[] weekends = {6, 7};
+    return weekends.Contains(weekDayNumber);
+}
+
+int weekDayNumber = getReadLineWeekDayNumber();
+if (isWeekend(weekDayNumber))
+{
+    Console.WriteLine($"{weekDayNumber} -> да");
+}
+else
+{
+    Console.WriteLine($"{weekDayNumber} -> нет");
+}
