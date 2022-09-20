8e18e4e81cb91ba191ae99527280ab3323bc77b2
Julia Debelova <u.deb@mail.ru>
2022-09-20 10:49:57 +0500

    add task06 from seminar 2 about random

---
+++ task06/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98
+int number = new Random().Next(100, 999);
+Console.Write(number);
+Console.Write(" -> ");
+Console.WriteLine((number - number % 100) / 10 + number %10);
