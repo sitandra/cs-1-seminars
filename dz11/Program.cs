5f3353a096c2c725ea048734cc2f81615facb25b
Julia Debelova <u.deb@mail.ru>
2022-09-20 12:53:41 +0500

    add dz11 from seminar 2 about functions: find digit on position 3 from number

---
+++ dz11/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+int getReadLineNumber(string message)
+{
+    int number;
+    Console.Write($"{message}: ");
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write($"Нет! {message}: ");
+    }
+    return number;
+}
+int? getDigitFromNumberOnPosition(int number, int digitPosition)
+{
+    int numberAbs = Math.Abs(number);
+    int numberLength = numberAbs.ToString().Length;
+    int? digit = null;
+    if (digitPosition <= numberLength && digitPosition > 1)
+    {
+        digitPosition = numberLength - digitPosition; // Переворачиваем нумерацию, потому что отсчитывать будем с конца
+        int currentPosition = 0;
+        while (currentPosition <= digitPosition)
+        {
+            digit = numberAbs % 10;
+            numberAbs = numberAbs / 10;
+            currentPosition ++;
+        }
+    }
+    return digit;
+}
+int number = getReadLineNumber("Введите число");
+int digitPosition = 3;
+int? digit = getDigitFromNumberOnPosition(number, digitPosition);
+if (digit == null)
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"{number} -> {digit}");
+}
