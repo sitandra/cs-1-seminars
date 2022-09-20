c0acd81c68e02b6d8f5a9613b67c946975735427
Julia Debelova <u.deb@mail.ru>
2022-09-20 12:52:52 +0500

    edit dz10 from seminar 2 about functions

--- dz10/Program.cs
+++ dz10/Program.cs
@@ -1,4 +1,4 @@
-﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 456 -> 5
 782 -> 8
@@ -18,7 +18,7 @@ int? getDigitFromNumberOnPosition(int number, int digitPosition)
     int numberAbs = Math.Abs(number);
     int numberLength = numberAbs.ToString().Length;
     int? digit = null;
-    if (digitPosition < numberLength && digitPosition > 1)
+    if (digitPosition <= numberLength && digitPosition > 1)
     {
         digitPosition = numberLength - digitPosition; // Переворачиваем нумерацию, потому что отсчитывать будем с конца
         int currentPosition = 0;
