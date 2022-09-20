da0c1d306591bd5c25e51db2f06b21670d36dc3a
Julia Debelova <u.deb@mail.ru>
2022-09-20 11:12:15 +0500

    add task08 from seminar 2 about % function

---
+++ task08/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
+bool isMultiple(int divisibleNumber, int divisor)
+{
+    return divisibleNumber % divisor == 0;
+}
+
+int number;
+Console.Write("Введите целое число: ");
+while(!int.TryParse(Console.ReadLine(), out number))
+{
+    Console.Write("Нет, введите целое число: ");
+}
+Console.Write(number);
+if (isMultiple(number, 7) && isMultiple(number, 23))
+{
+    Console.Write(" -> да");
+}
+else
+{
+    Console.Write(" -> нет");
+}
