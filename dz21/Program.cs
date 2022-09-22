5cc7274e9678a363a6dbae776c5b4ba17d421a63
Julia Debelova <u.deb@mail.ru>
2022-09-23 00:41:18 +0500

    add dz21 – task about palindrome

---
+++ dz21/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+int getNumberFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation}: ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result < 10000 || result > 99999)
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое пятизначное число. {userInformation}: ");
+    }
+    return result;
+}
+
+bool isPalindrome(int number)
+{
+    return number/1000 == number%10 * 10 + number%100 / 10;
+}
+
+int userNumber = getNumberFromUser("Введите число");
+string answer = isPalindrome(userNumber) ? "да" : "нет";
+Console.WriteLine($"{userNumber} -> {answer}");
