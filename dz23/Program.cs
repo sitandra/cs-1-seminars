ee31786a1f482e7cdf623686eb31e0be2bfa8700
Julia Debelova <u.deb@mail.ru>
2022-09-23 00:44:24 +0500

    add dz23 – task about table of cubes

---
+++ dz23/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+int getNumberFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result <= 1)
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число больше единицы. {userInformation} ");
+    }
+    return result;
+}
+
+int[] tableOfCubes(int number)
+{
+    int[] result = new int [number];
+    for(int i = 1; i <= number; i++)
+    {
+        result[i - 1] = (int)(Math.Pow(i, 3));
+    }
+    return result;
+}
+
+
+int number = getNumberFromUser("Введите целое число N > 1: ");
+int[] table = tableOfCubes(number);
+
+foreach (var value in table)
+{
+    Console.Write(value + " ");
+}
