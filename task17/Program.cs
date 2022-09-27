c5efa798788e75a9981c2f2bd1410fdf2eafdfc1
Julia Debelova <u.deb@mail.ru>
2022-09-27 12:34:30 +0500

    add task17 about array and random

---
+++ task17/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0] */
+
+byte[] InitBoolArray(int count)
+{
+    byte[] result = new byte[count];
+    for (int i = 0; i < count; i++)
+    {
+        result[i] = (byte)new Random().Next(0, 2);
+    }
+    return result;
+}
+
+void PrintArray(byte[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(i == 0 ? "" : ", ");
+        Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+PrintArray(InitBoolArray(8));
