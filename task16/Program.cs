e2204bf2a988ff0396f29501aef81f30d1df97fa
Julia Debelova <u.deb@mail.ru>
2022-09-27 12:09:46 +0500

    add task16 about function, for

---
+++ task16/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120 */
+int GetNumberFromUser(string userInformation)
+{
+    int result;
+    Console.Write($"{userInformation}: ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result < 1)
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число больше нуля. {userInformation}: ");
+    }
+    return result;
+}
+
+int GetMultiplicationOfRange(int startNumber, int endNumber)
+{
+    int result = Math.Min(startNumber, endNumber);
+    int max = Math.Max(startNumber, endNumber);
+    for (int i = result + 1; i <= max; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+int number = GetNumberFromUser("Введите число N");
+Console.WriteLine($"{number} -> {GetMultiplicationOfRange(number,1)}");
