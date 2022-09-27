8bf37afaf77df5db78571ae59939738d6838a953
Julia Debelova <u.deb@mail.ru>
2022-09-27 11:12:51 +0500

    add task14 about function & for

---
+++ task14/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36 */
+
+int GetNumberFromUser(string userInformation)
+{
+    int result;
+    Console.Write($"{userInformation}: ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result < 1)
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число больше нуля. {userInformation}: ");
+    }
+    return result;
+}
+
+int Sum(int number)
+{
+    int result = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+int number = GetNumberFromUser("Введите число A");
+Console.WriteLine($"{number} -> {Sum(number)}");
