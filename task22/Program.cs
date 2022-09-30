6ad09f257f85022636f3054dc84e857474861b3a
Julia Debelova <u.deb@mail.ru>
2022-09-30 11:06:58 +0500

    add task22 about arrays

---
+++ task22/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21 */
+
+int[] InitRandomArray(int length, int start, int end)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(start, end + 1);
+    }
+    return result;
+}
+
+void PrintArray(int[] array, bool newLine = true)
+{
+    Console.Write($"[{array[0]}");
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($", {array[i]}");
+    }
+    if(newLine) Console.WriteLine("]");
+    else Console.Write("]");
+}
+
+int[] GetMagic(int[] numbers)
+{
+    int[] result = new int[numbers.Length % 2 == 0 ? numbers.Length / 2 : numbers.Length / 2 + 1];
+    int lastElement = numbers.Length - 1;
+    for (int i = 0; i < numbers.Length / 2; i++)
+    {
+        result[i] = numbers[i] * numbers[lastElement - i];
+    }
+    if (numbers.Length % 2 != 0) result[result.Length - 1] = numbers[numbers.Length / 2];
+    return result;
+}
+
+int[] numbers = InitRandomArray(5, 0, 10);
+PrintArray(numbers, false);
+Console.Write($" -> ");
+PrintArray(GetMagic(numbers));
