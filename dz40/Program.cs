bf66a39aab77db1d55f6d597760b08e7a61501ea
Julia Debelova <u.deb@mail.ru>
2022-09-30 11:38:46 +0500

    add dz40 about count of even numbers in array

---
+++ dz40/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int[] InitRandomArray(int length, int start, int end)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(start, end + 1);
+    }
+    return result;
+}
+
+void PrintArray(int[] array, bool newLine = true)
+{
+    Console.Write($"[{array[0]}");
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($", {array[i]}");
+    }
+    if (newLine) Console.WriteLine("]");
+    else Console.Write("]");
+}
+
+int CountOfEvenNumbers(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] % 2 == 0) count ++;
+    }
+    return count;
+}
+
+int[] numbers = InitRandomArray(4, 100, 999);
+PrintArray(numbers, false);
+Console.WriteLine($" -> {CountOfEvenNumbers(numbers)}");
