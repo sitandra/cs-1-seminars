d00e3d1a12efe3dc19e6e30ab18b8174283dc335
Julia Debelova <u.deb@mail.ru>
2022-09-30 10:42:28 +0500

    add task20 about arrays

---
+++ task20/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да
+*/
+
+int[] InitRandomArray(int length, int start, int end)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(start, end + 1);
+    }
+    return result;
+}
+
+void PrintArray(int[] array, bool newLine = true)
+{
+    Console.Write($"[{array[0]}");
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($", {array[i]}");
+    }
+    if(newLine) Console.WriteLine("]");
+    else Console.Write("]");
+}
+
+void PrintInConsoleWithColor(string message, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(message);
+    Console.ResetColor();
+}
+
+int GetNumberFromUser(string userInformation)
+{
+    int result;
+    PrintInConsoleWithColor($"{userInformation}: ", ConsoleColor.DarkBlue);
+    while (!int.TryParse(Console.ReadLine(), out result))
+    {
+        PrintInConsoleWithColor($"Ошибка ввода! Ожидается целое число. {userInformation}: ", ConsoleColor.DarkYellow);
+    }
+    return result;
+}
+
+bool IsExistsInArray(int[] numbers, int number)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] == number) return true;
+    }
+    return false;
+}
+
+int[] numbers = InitRandomArray(5, 0, 255);
+PrintArray(numbers);
+int number = GetNumberFromUser("Введите число");
+Console.Write("Массив ");
+PrintArray(numbers, false);
+Console.WriteLine($" -> {(IsExistsInArray(numbers, number) ? "да" : "нет")}");
