36c4e615eff339233b1041919c8bf48b08046e3f
Julia Debelova <u.deb@mail.ru>
2022-10-04 00:02:56 +0500

    add task27 about copy array by element

---
+++ task27/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования. */
+double[] copyArray(double[] array)
+{
+    double[] arr = new double[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arr[i] = array[i];
+    }
+    return arr;
+}
+
+void PrintArray(double[] array, bool newLine = true)
+{
+    Console.Write($"[{array[0]}");
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($", {array[i]}");
+    }
+    if (newLine) Console.WriteLine("]");
+    else Console.Write("]");
+}
+
+double[] array = { 2, 4, 8.3, 10 };
+double[] arrayCopy = copyArray(array);
+
+PrintArray(array);
+PrintArray(arrayCopy);
