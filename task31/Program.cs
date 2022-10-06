11bcbde112b9090c42526c84ee18c37b81205e03
Julia Debelova <u.deb@mail.ru>
2022-10-07 00:10:06 +0500

    add task31 about matrix array and random

---
+++ task31/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+
+void PrintInConsoleWithColor(string message, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(message);
+    Console.ResetColor();
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.Write(" \t");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        PrintInConsoleWithColor($"{j}\t", ConsoleColor.Green);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        PrintInConsoleWithColor($"{i} \t", ConsoleColor.Green);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] InitRandomMatrix(int height, int width, int leftRangeValue, int rightRangeValue)
+{
+    int[,] matrix = new int[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            matrix[i, j] = new Random().Next(leftRangeValue, rightRangeValue + 1);
+        }
+    }
+    return matrix;
+}
+
+int n = 5;
+int m = 4;
+
+int[,] matrix = InitRandomMatrix(m, n, -100, 100);
+PrintMatrix(matrix);
