98bed362db1d2c36a6a929afdadc5bbe40a308a6
Julia Debelova <u.deb@mail.ru>
2022-10-07 00:21:11 +0500

    add dz60 about random float matrix

---
+++ dz60/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+
+*/
+
+void PrintInConsoleWithColor(string message, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(message);
+    Console.ResetColor();
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    Console.Write(" \t");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        PrintInConsoleWithColor($"{j}\t", ConsoleColor.Green);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        PrintInConsoleWithColor($"{i} \t", ConsoleColor.Green);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] InitRandomMatrix(int height, int width, int leftRangeValue, int rightRangeValue)
+{
+    double[,] matrix = new double[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            matrix[i, j] = (double)(new Random().Next(leftRangeValue * 10, rightRangeValue * 10 + 1)) / 10;
+        }
+    }
+    return matrix;
+}
+
+int n = 4;
+int m = 3;
+
+double[,] matrix = InitRandomMatrix(m, n, -10, 10);
+PrintMatrix(matrix);
