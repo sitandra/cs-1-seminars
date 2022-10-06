b4c12de34c28da147d326a25f78e5106edc22298
Julia Debelova <u.deb@mail.ru>
2022-10-07 00:50:52 +0500

    add dz61 about find element in matrix by number of order

---
+++ dz61/Program.cs
@@ -0,0 +1,77 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позицию элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+5 -> 9
+
+индексы:
+[0][1][2][3]
+1 4 7 2
+[4][5][6][7]
+5 9 2 3
+[8][9][10][11]
+8 4 2 4
+
+*/
+
+void PrintInConsoleWithColor(string message, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(message);
+    Console.ResetColor();
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.Write(" \t");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        PrintInConsoleWithColor($"{j}\t", ConsoleColor.Red);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        PrintInConsoleWithColor($"{i} \t", ConsoleColor.Red);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetindexFromUser(string userInformation)
+{
+    int result;
+    PrintInConsoleWithColor($"{userInformation}: ", ConsoleColor.DarkBlue);
+    while (!int.TryParse(Console.ReadLine(), out result) || result <= 0)
+    {
+        PrintInConsoleWithColor($"Ошибка ввода! Ожидается число больше 0. {userInformation}: ", ConsoleColor.DarkYellow); ;
+    }
+    return result;
+}
+
+int? GetValueFromMatrixByPosition(int[,] matrix, int position)
+{
+    int width = matrix.GetLength(1);
+    int maxIndex = matrix.GetLength(0) * width;
+    if (maxIndex < position) return null;
+    int row = (int)Math.Ceiling((double)position / (double)width) - 1;
+    int col = position - (row * width + 1);
+    return matrix[row, col];
+}
+
+int[,] matrix = {
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+PrintMatrix(matrix);
+int position = GetindexFromUser("Введите позицию элемента");
+int? number = GetValueFromMatrixByPosition(matrix, position);
+if (number == null) Console.WriteLine($"{position} -> такого числа в массиве нет");
+else  Console.WriteLine($"{position} -> {number}");
