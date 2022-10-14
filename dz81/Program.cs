373e0d2325489a208456786120dff2123d5d6e8d
Julia Debelova <u.deb@mail.ru>
2022-10-14 19:10:49 +0500

    add dz81 about recursion sum from natural numbers range

---
+++ dz81/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Все задачи решаются через Рекурсию
+
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+
+void PrintInConsoleWithColor(string message, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(message);
+    Console.ResetColor();
+}
+
+int GetNumberFromUser(string userInformation)
+{
+    int result;
+    PrintInConsoleWithColor($"{userInformation}: ", ConsoleColor.DarkBlue);
+    while (!int.TryParse(Console.ReadLine(), out result) || result < 1)
+    {
+        PrintInConsoleWithColor($"Ошибка ввода! Ожидается натуральное число больше 1. {userInformation}: ", ConsoleColor.DarkYellow); ;
+    }
+    return result;
+}
+
+int GetSumNaturalNumbers(int firstNumber, int lastNumber)
+{
+    if (lastNumber == firstNumber) {
+        return lastNumber;
+    }
+    return lastNumber + GetSumNaturalNumbers(firstNumber, lastNumber - 1);
+}
+
+int firstNumber = GetNumberFromUser("Введите число M");
+int lastNumber = GetNumberFromUser("Введите число N");
+if (lastNumber < firstNumber) (lastNumber, firstNumber) = (firstNumber, lastNumber);
+int sum = GetSumNaturalNumbers(firstNumber, lastNumber);
+Console.WriteLine($"M = {firstNumber}; N = {lastNumber} -> {sum}");
